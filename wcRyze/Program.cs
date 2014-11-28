7024a27827eab09356f44a9f22c3ba77ad69d7c3
Royalsaint <fulldesignerbr@hotmail.com>
2014-11-28 00:24:27 -0200

    wcRyze
    wcRyze

---
+++ wcRyze/Program.cs
@@ -0,0 +1,297 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+#endregion
+
+namespace wcRyze
+{
+    internal class Program
+    {
+        public const string ChampionName = "Ryze";
+
+        public static Orbwalking.Orbwalker Orbwalker;
+
+        public static List<Spell> SpellList = new List<Spell>();
+
+        public static Spell Q;
+        public static Spell W;
+        public static Spell E;
+
+        public static Menu Config;
+
+        private static Obj_AI_Hero Player;
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Player = ObjectManager.Player;
+            if (Player.BaseSkinName != ChampionName) return;
+
+            Q = new Spell(SpellSlot.Q, 625);
+            W = new Spell(SpellSlot.W, 600);
+            E = new Spell(SpellSlot.E, 600);
+
+            SpellList.Add(Q);
+            SpellList.Add(W);
+            SpellList.Add(E);
+
+            Config = new Menu(ChampionName, ChampionName, true);
+
+            var targetSelectorMenu = new Menu("Target Selector", "Seletor de Targeto");
+            SimpleTs.AddToMenu(targetSelectorMenu);
+            Config.AddSubMenu(targetSelectorMenu);
+
+            Config.AddSubMenu(new Menu("Orbwalking", "LXOrbwalking"));
+
+            Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
+
+            Config.AddSubMenu(new Menu("Combo", "Combo"));
+            Config.SubMenu("Combo")
+                .AddItem(new MenuItem("ComboActive", "Combo!").SetValue(new KeyBind(32, KeyBindType.Press)));
+
+            Config.AddSubMenu(new Menu("Harass", "Harass"));
+            Config.SubMenu("Harass").AddItem(new MenuItem("UseQHarass", "Usar Q").SetValue(true));
+            Config.SubMenu("Harass")
+                .AddItem(
+                    new MenuItem("HarassActive", "Harass!").SetValue(new KeyBind("C".ToCharArray()[0], KeyBindType.Press)));
+
+            Config.AddSubMenu(new Menu("Farm", "Farm"));
+            Config.SubMenu("Farm")
+                .AddItem(
+                    new MenuItem("UseQFarm", "Usar Q").SetValue(
+                        new StringList(new[] { "Freeze", "LaneClear", "Ambos", "Nao" }, 2)));
+            Config.SubMenu("Farm")
+                .AddItem(
+                    new MenuItem("UseWFarm", "Usar W").SetValue(
+                        new StringList(new[] { "Freeze", "LaneClear", "Ambos", "Nao" }, 3)));
+            Config.SubMenu("Farm")
+                .AddItem(
+                    new MenuItem("UseEFarm", "Usar E").SetValue(
+                        new StringList(new[] { "Freeze", "LaneClear", "Ambos", "Nao" }, 1)));
+            Config.SubMenu("Farm")
+                .AddItem(
+                    new MenuItem("FreezeActive", "Freeze!").SetValue(new KeyBind("C".ToCharArray()[0], KeyBindType.Press)));
+            Config.SubMenu("Farm")
+                .AddItem(
+                    new MenuItem("LaneClearActive", "LaneClear!").SetValue(new KeyBind("V".ToCharArray()[0],
+                        KeyBindType.Press)));
+
+            Config.AddSubMenu(new Menu("JungleFarm", "Jungle"));
+            Config.SubMenu("JungleFarm").AddItem(new MenuItem("UseQJFarm", "Usar Q").SetValue(true));
+            Config.SubMenu("JungleFarm").AddItem(new MenuItem("UseWJFarm", "Usar W").SetValue(true));
+            Config.SubMenu("JungleFarm").AddItem(new MenuItem("UseEJFarm", "Usar E").SetValue(true));
+            Config.SubMenu("JungleFarm")
+                .AddItem(
+                    new MenuItem("JungleFarmActive", "JungleFarm!").SetValue(new KeyBind("V".ToCharArray()[0],
+                        KeyBindType.Press)));
+
+            //Damage after combo:
+            var dmgAfterComboItem = new MenuItem("DamageAfterCombo", "Mostrar dano depois do combo").SetValue(true);
+            Utility.HpBarDamageIndicator.DamageToUnit += hero => (float)(Player.GetSpellDamage(hero, SpellSlot.Q) * 2 + Player.GetSpellDamage(hero, SpellSlot.W) + Player.GetSpellDamage(hero, SpellSlot.E));
+            Utility.HpBarDamageIndicator.Enabled = dmgAfterComboItem.GetValue<bool>();
+            dmgAfterComboItem.ValueChanged += delegate(object sender, OnValueChangeEventArgs eventArgs)
+            {
+                Utility.HpBarDamageIndicator.Enabled = eventArgs.GetNewValue<bool>();
+            };
+
+            Config.AddSubMenu(new Menu("Drawings", "Drawings"));
+            Config.SubMenu("Drawings")
+                .AddItem(new MenuItem("QRange", "Alcance do Q").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
+            Config.SubMenu("Drawings")
+                .AddItem(
+                    new MenuItem("WRange", "Alcance do W").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
+            Config.SubMenu("Drawings")
+                .AddItem(
+                    new MenuItem("ERange", "Alcance do E").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
+            Config.SubMenu("Drawings").AddItem(dmgAfterComboItem);
+            Config.AddToMainMenu();
+
+            //Add the events we are going to use:
+            Drawing.OnDraw += Drawing_OnDraw;
+            Game.OnGameUpdate += Game_OnGameUpdate;
+            Orbwalking.BeforeAttack += OrbwalkingOnBeforeAttack;
+        }
+
+        private static void OrbwalkingOnBeforeAttack(Orbwalking.BeforeAttackEventArgs args)
+        {
+            if (Config.Item("ComboActive").GetValue<KeyBind>().Active)
+                args.Process = !(Q.IsReady() || W.IsReady() || E.IsReady() || Player.Distance(args.Target) >= 600);
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            //Draw the ranges of the spells.
+            foreach (var spell in SpellList)
+            {
+                var menuItem = Config.Item(spell.Slot + "Range").GetValue<Circle>();
+                if (menuItem.Active)
+                {
+                    Utility.DrawCircle(Player.Position, spell.Range, menuItem.Color);
+                }
+            }
+        }
+
+        private static void Game_OnGameUpdate(EventArgs args)
+        {
+            if (Config.Item("ComboActive").GetValue<KeyBind>().Active)
+            {
+                Combo();
+            }
+            else
+            {
+                if (Config.Item("HarassActive").GetValue<KeyBind>().Active)
+                    Harass();
+
+                var lc = Config.Item("LaneClearActive").GetValue<KeyBind>().Active;
+                if (lc || Config.Item("FreezeActive").GetValue<KeyBind>().Active)
+                    Farm(lc);
+
+                if (Config.Item("JungleFarmActive").GetValue<KeyBind>().Active)
+                    JungleFarm();
+            }
+        }
+
+        private static void Combo()
+        {
+            var target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Magical);
+            var qCd = Player.Spellbook.GetSpell(SpellSlot.Q).CooldownExpires - Game.Time;
+
+            if (target != null)
+            {
+                if (Player.Distance(target) <= 600)
+                {
+                    if (Player.Distance(target) >= 575 && W.IsReady() && target.Path.Count() > 0 &&
+                        target.Path[0].Distance(Player.ServerPosition) >
+                        Player.Distance(target))
+                    {
+                        W.CastOnUnit(target);
+                    }
+                    else if (Q.IsReady())
+                    {
+                        Q.CastOnUnit(target);
+                    }
+                    else
+                    {
+                        if (qCd > 1.25f)
+                        {
+                            if (W.IsReady())
+                            {
+                                W.CastOnUnit(target);
+                            }
+                            else if (E.IsReady())
+                            {
+                                E.CastOnUnit(target);
+                            }
+                        }
+                    }
+                }
+                else if (Player.GetSpellDamage(target, SpellSlot.Q) > target.Health)
+                {
+                    Q.CastOnUnit(target);
+                }
+            }
+        }
+
+        private static void Harass()
+        {
+            var target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Magical);
+
+            if (target != null && Config.Item("UseQHarass").GetValue<bool>())
+            {
+                Q.CastOnUnit(target);
+            }
+        }
+
+        private static void Farm(bool laneClear)
+        {
+            if (!Orbwalking.CanMove(40)) return;
+            var allMinions = MinionManager.GetMinions(Player.ServerPosition, Q.Range);
+            var useQi = Config.Item("UseQFarm").GetValue<StringList>().SelectedIndex;
+            var useWi = Config.Item("UseWFarm").GetValue<StringList>().SelectedIndex;
+            var useEi = Config.Item("UseEFarm").GetValue<StringList>().SelectedIndex;
+            var useQ = (laneClear && (useQi == 1 || useQi == 2)) || (!laneClear && (useQi == 0 || useQi == 2));
+            var useW = (laneClear && (useWi == 1 || useWi == 2)) || (!laneClear && (useWi == 0 || useWi == 2));
+            var useE = (laneClear && (useEi == 1 || useEi == 2)) || (!laneClear && (useEi == 0 || useEi == 2));
+
+            if (useQ && Q.IsReady())
+            {
+                foreach (var minion in allMinions)
+                {
+                    if (minion.IsValidTarget() &&
+                        HealthPrediction.GetHealthPrediction(minion,
+                            (int)(Player.Distance(minion) * 1000 / 1400)) <
+                         Player.GetSpellDamage(minion, SpellSlot.Q))
+                    {
+                        Q.CastOnUnit(minion);
+                        return;
+                    }
+                }
+            }
+            else if (useW && W.IsReady())
+            {
+                foreach (var minion in allMinions)
+                {
+                    if (minion.IsValidTarget(W.Range) &&
+                        minion.Health < Player.GetSpellDamage(minion, SpellSlot.Q))
+                    {
+                        W.CastOnUnit(minion);
+                        return;
+                    }
+                }
+            }
+            else if (useE && E.IsReady())
+            {
+                foreach (var minion in allMinions)
+                {
+                    if (minion.IsValidTarget(E.Range) &&
+                        HealthPrediction.GetHealthPrediction(minion,
+                            (int)(Player.Distance(minion) * 1000 / 1000)) <
+                        Player.GetSpellDamage(minion, SpellSlot.Q) - 10)
+                    {
+                        E.CastOnUnit(minion);
+                        return;
+                    }
+                }
+            }
+
+            if (laneClear)
+            {
+                foreach (var minion in allMinions)
+                {
+                    if (useQ)
+                        Q.CastOnUnit(minion);
+
+                    if (useW)
+                        W.CastOnUnit(minion);
+
+                    if (useE)
+                        E.CastOnUnit(minion);
+                }
+            }
+        }
+
+        private static void JungleFarm()
+        {
+            var mobs = MinionManager.GetMinions(Player.ServerPosition, Q.Range,
+                MinionTypes.All,
+                MinionTeam.Neutral, MinionOrderTypes.MaxHealth);
+            if (mobs.Count > 0)
+            {
+                var mob = mobs[0];
+                Q.CastOnUnit(mob);
+                W.CastOnUnit(mob);
+                E.CastOnUnit(mob);
+            }
+        }
+    }
+}
